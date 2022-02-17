e541913bc86a717a38e4e037b03637869dcbbd72
Lucas Ebert <lucascello@yahoo.com.br>
2022-02-17 16:57:21 +0100

    geolocation

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => console.log("position :)", position),
+        (err) => console.log(err)
+    );
     return <div>Hi There!</div>;
 };
 
