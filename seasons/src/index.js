c64041a12dba64aaf73d416fba36c3d11034dc24
Lucas Ebert <lucascello@yahoo.com.br>
2022-02-17 17:15:36 +0100

    chage to class component

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,12 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log("position :)", position),
-        (err) => console.log(err)
-    );
-    return <div>Hi There!</div>;
-};
+// const App = () => {
+//     window.navigator.geolocation.getCurrentPosition(
+//         (position) => console.log("position :)", position),
+//         (err) => console.log(err)
+//     );
+//     return <div>Hi There!</div>;
+// };
+
+class App extends React.Component {
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log("position :)", position),
+            (err) => console.log(err)
+        );
+        return <div>Latitude: </div>;
+    }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
