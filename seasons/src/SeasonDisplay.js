c181408b8820901584d2657f5a3ccbd3dbfaaf71
Lucas Ebert <lucascello@yahoo.com.br>
2022-02-17 20:09:29 +0100

    added some icons

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -11,13 +11,31 @@ const getSeason = (latitude, month) => {
 const SeasonDisplay = (props) => {
     const season = getSeason(props.latitude, new Date().getMonth);
     // console.log("Whats the season?", season);
+    const text =
+        season === "winter" ? "Burr, it's chilly" : "Let's hit the beach";
+    const icon = season === "winter" ? "snowflake" : "sun";
 
     // console.log("Latitude in the SeasonDisplay component:", props.latitude);
     return (
         <div>
-            {season === "winter" ? "Burr, it's chilly" : "Let's hit the beach"}
+            {/* <h1>
+                {season === "winter"
+                    ? "Burr, it's chilly"
+                    : "Let's hit the beach"}
+            </h1> */}
+            {/* better way of writting h1 the code above */}
+            <i className={`${icon} icon`} />
+            <h1>{text}</h1>
+            <i className={`${icon} icon`} />
         </div>
     );
 };
 
 export default SeasonDisplay;
+
+{
+    /* <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css"
+/>; */
+}
