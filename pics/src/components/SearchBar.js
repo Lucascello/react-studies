4de9e2a1ff1f8762fbdf5946b88735593da937a2
Lucas Ebert <lucascello@yahoo.com.br>
2022-02-18 16:18:34 +0100

    controled output

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,9 +1,10 @@
 import React from "react";
 
 export default class SearchBar extends React.Component {
-    onInputChange(e) {
-        console.log(e.target.value);
-    }
+    state = { term: "" };
+    // onInputChange(e) {
+    //     console.log(e.target.value);
+    // }
 
     render() {
         return (
@@ -11,7 +12,18 @@ export default class SearchBar extends React.Component {
                 <form className="ui form">
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text" onChange={this.onInputChange} />
+                        {/* <input
+                            type="text"
+                            onChange={(e) => console.log(e.target.value)}
+                        /> */}
+                        {/* <input type="text" onChange={this.onInputChange} /> */}
+                        <input
+                            type="text"
+                            value={this.state.term}
+                            onChange={(e) =>
+                                this.setState({ term: e.target.value })
+                            }
+                        />
                     </div>
                 </form>
             </div>
