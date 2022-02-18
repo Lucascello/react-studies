3b2c6579ee5a27900a1fbf1bb20be98b2ea4d292
Lucas Ebert <lucascello@yahoo.com.br>
2022-02-18 18:21:47 +0100

    AJAX (axios request) to unsplash

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,9 +1,17 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
     onSearchSubmit(term) {
-        console.log("term on the App component: ", term);
+        // console.log("term on the App component: ", term);
+        axios.get("https://api.unsplash.com/search/photos", {
+            params: { query: term },
+            headers: {
+                Authorization:
+                    "Client-ID CbLObj9qjPIOgSdJBOYtX46LmQMrfYfSeXYK3UaLHw4",
+            },
+        });
     }
 
     render() {
