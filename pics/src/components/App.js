b3aae81aab11f2eedc95e7766238b197aa68cbf4
Lucas Ebert <lucascello@yahoo.com.br>
2022-02-18 20:02:43 +0100

    using async await promises

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -3,14 +3,29 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-    onSearchSubmit(term) {
+    async onSearchSubmit(term) {
         // console.log("term on the App component: ", term);
-        axios.get("https://api.unsplash.com/search/photos", {
-            params: { query: term },
-            headers: {
-                Authorization: `Client-ID ${process.env.REACT_APP_PICS_API_KEY}`,
-            },
-        });
+        const response = await axios.get(
+            "https://api.unsplash.com/search/photos",
+            {
+                params: { query: term },
+                headers: {
+                    Authorization: `Client-ID ${process.env.REACT_APP_PICS_API_KEY}`,
+                },
+            }
+        );
+        console.log("response from unsplash", response.data.results);
+
+        // axios
+        //     .get("https://api.unsplash.com/search/photos", {
+        //         params: { query: term },
+        //         headers: {
+        //             Authorization: `Client-ID ${process.env.REACT_APP_PICS_API_KEY}`,
+        //         },
+        //     })
+        //     .then((response) => {
+        //         console.log("response from unsplash", response.data.results);
+        //     });
     }
 
     render() {
