411dfdc3b93b2d8ad65ebf329c49d0d550cd4851
Lucas Ebert <lucascello@yahoo.com.br>
2022-02-21 13:34:09 +0100

    using a new component for the CSS, and for each image

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,12 +1,24 @@
 import React from "react";
+import "./ImageList.css";
+import ImageCard from "./ImageCard";
 
 const ImageList = (props) => {
     // console.log("image props from App: ", props.images);
+    // const images = props.images.map((image) => {
+    //     return <img alt={image.description} key={image.id} src={image.urls.regular} />;
+    // });
+
+    // This looks better than the code above, as I destructure the images object
+    // const images = props.images.map(({ description, id, urls }) => {
+    //     return <img alt={description} key={id} src={urls.regular} />;
+    // });
+
+    // Now doing the above version, passing a component to it
     const images = props.images.map((image) => {
-        return <img src={image.urls.regular} />;
+        return <ImageCard key={image.id} image={image} />;
     });
 
-    return <div>{images}</div>;
+    return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
