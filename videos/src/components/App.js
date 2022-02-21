237d4babefb1eadf87e05fbd0a75fa17b88aabef
Lucas Ebert <lucascello@yahoo.com.br>
2022-02-21 20:24:17 +0100

    stiled the video's detail

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../api/youtube";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
     state = { videos: [], selectedVideo: null };
@@ -18,13 +19,15 @@ class App extends React.Component {
 
     onVideoSelect = (video) => {
         // console.log("Video from the app component: ", video);
-        
+
+        this.setState({ selectedVideo: video });
     };
 
     render() {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit} />
+                <VideoDetail video={this.state.selectedVideo} />
                 <VideoList
                     onVideoSelect={this.onVideoSelect}
                     videos={this.state.videos}
