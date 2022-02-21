07efee1f188d7b61c1a065c3b70c62e702c705c9
Lucas Ebert <lucascello@yahoo.com.br>
2022-02-21 20:12:42 +0100

    prepared state for clicking on individual videos

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -4,7 +4,7 @@ import youtube from "../api/youtube";
 import VideoList from "./VideoList";
 
 class App extends React.Component {
-    state = { videos: [] };
+    state = { videos: [], selectedVideo: null };
 
     onTermSubmit = async (term) => {
         // console.log("Term from the SearchBar component: ", term);
@@ -16,11 +16,19 @@ class App extends React.Component {
         this.setState({ videos: response.data.items });
     };
 
+    onVideoSelect = (video) => {
+        // console.log("Video from the app component: ", video);
+        
+    };
+
     render() {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit} />
-                <VideoList videos={this.state.videos} />
+                <VideoList
+                    onVideoSelect={this.onVideoSelect}
+                    videos={this.state.videos}
+                />
             </div>
         );
     }
