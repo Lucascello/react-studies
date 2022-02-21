8b1f089646577c36b9162aacd34fb1dbed4a63c6
Lucas Ebert <lucascello@yahoo.com.br>
2022-02-21 12:15:42 +0100

    got all images showing

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,9 +1,12 @@
 import React from "react";
 
 const ImageList = (props) => {
-    console.log("image props from App: ", props.images);
+    // console.log("image props from App: ", props.images);
+    const images = props.images.map((image) => {
+        return <img src={image.urls.regular} />;
+    });
 
-    return <div>Image List</div>;
+    return <div>{images}</div>;
 };
 
 export default ImageList;
