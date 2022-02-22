22215b4d5ebe9ce2d4e2ccd4b9bb6aab403c5755
Lucas Ebert <lucascello@yahoo.com.br>
2022-02-22 15:14:41 +0100

    axios request getting the right data

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -3,21 +3,30 @@ import axios from "axios";
 
 const Search = () => {
     const [term, setTerm] = useState("");
+    const [results, setResults] = useState([]);
+
+    console.log("results from the Search: ", results);
 
     useEffect(() => {
         const search = async () => {
-            await axios.get("https://en.wikipedia.org/w/api.php", {
-                params: {
-                    action: "query",
-                    list: "search",
-                    origin: "*",
-                    format: "json",
-                    srsearch: term,
-                },
-            });
+            const { data } = await axios.get(
+                "https://en.wikipedia.org/w/api.php",
+                {
+                    params: {
+                        action: "query",
+                        list: "search",
+                        origin: "*",
+                        format: "json",
+                        srsearch: term,
+                    },
+                }
+            );
+            setResults(data.query.search);
         };
 
-        search();
+        if (term) {
+            search();
+        }
     }, [term]);
 
     return (
