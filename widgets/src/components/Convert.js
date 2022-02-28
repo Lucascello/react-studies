78d4b10733e9585477fa03bb15d632e601295013
Lucas Ebert <lucascello@yahoo.com.br>
2022-02-28 19:27:22 +0100

    translation working

--- widgets/src/components/Convert.js
+++ widgets/src/components/Convert.js
@@ -2,22 +2,44 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Convert = ({ language, text }) => {
+    const [translated, setTranslated] = useState("");
+    const [debouncedText, setDebouncedText] = useState(text);
+
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedText(text);
+        }, 500);
+
+        return () => {
+            clearTimeout(timerId);
+        };
+    }, [text]);
+
     useEffect(() => {
         // console.log("New language or text");
-        axios.post(
-            "https://translation.googleapis.com/language/translate/v2",
-            {},
-            {
-                params: {
-                    q: text,
-                    target: language.value,
-                    key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM",
-                },
-            }
-        );
-    }, [language, text]);
+        const doTranslation = async () => {
+            const { data } = await axios.post(
+                "https://translation.googleapis.com/language/translate/v2",
+                {},
+                {
+                    params: {
+                        q: debouncedText,
+                        target: language.value,
+                        key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM",
+                    },
+                }
+            );
 
-    return <div>Convert</div>;
+            setTranslated(data.data.translations[0].translatedText);
+        };
+        doTranslation();
+    }, [language, debouncedText]);
+
+    return (
+        <div>
+            <h1 className="ui header">{translated}</h1>
+        </div>
+    );
 };
 
 export default Convert;
