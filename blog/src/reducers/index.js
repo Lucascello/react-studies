8f04221b9c721d170861d0dd6d1a149f08113fc8
Lucas Ebert <lucascello@yahoo.com.br>
2022-03-23 17:03:05 +0100

    blog project start

---
+++ blog/src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+
+export default combineReducers({
+    //Just a dummy reducer below to stop the error message on the console, until I know which reducers I need
+    replaceMe: () => "hi there",
+});
