67e47643ab5dfd8c642583f0c99a6b4374f2c15d
Lucas Ebert <lucascello@yahoo.com.br>
2022-03-23 19:03:13 +0100

    async action creator

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,5 +1,33 @@
-export const fetchPosts = () => {
-    return {
+import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+// export const fetchPosts = async () => {
+//     // Bad approach!!! Doesn't work
+//     const promise = await jsonPlaceholder.get("posts");
+
+//     return {
+//         type: "FETCH_POSTS",
+//         payload: promise,
+//     };
+// };
+
+// This is the same as the code below, but refactored
+
+export const fetchPosts = () => async (dispatch) => {
+    const response = await jsonPlaceholder.get("posts");
+
+    dispatch({
         type: "FETCH_POSTS",
-    };
+        payload: response,
+    });
 };
+
+// export const fetchPosts = () => {
+//     return async function (dispatch, getState) {
+//         const response = await jsonPlaceholder.get("posts");
+
+//         dispatch({
+//             type: "FETCH_POSTS",
+//             payload: response,
+//         });
+//     };
+// };
