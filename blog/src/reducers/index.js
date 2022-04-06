0246db168d2476c1146b3befbb3609fd7adc3bc6
Lucas Ebert <lucascello@yahoo.com.br>
2022-04-06 18:18:12 +0200

    starting to write the reducer

--- blog/src/reducers/index.js
+++ blog/src/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from "redux";
+import postsReducer from "./postsReducer";
 
 export default combineReducers({
     //Just a dummy reducer below to stop the error message on the console, until I know which reducers I need
-    replaceMe: () => "hi there",
+    // replaceMe: () => "hi there",
+
+    posts: postsReducer,
 });
