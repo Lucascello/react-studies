689ab40e799457d20ebb016bba57377112b2393d
Lucas Ebert <lucascello@yahoo.com.br>
2022-04-08 14:41:24 +0200

    code refactor with the -:chain() method

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -9,6 +9,14 @@ export const fetchPostsAndUsers = () => async (dispatch, getState) => {
     const userIds = _.uniq(_.map(getState().posts, "userId"));
 
     userIds.forEach((id) => dispatch(fetchUser(id)));
+
+    // refactoring the code above with _.chain() method
+
+    // _.chain(getState().posts)
+    //     .map("userId")
+    //     .uniq()
+    //     .forEach((id) => dispatch(fetchUser(id)))
+    //     .value();
 };
 
 // export const fetchPosts = async () => {
