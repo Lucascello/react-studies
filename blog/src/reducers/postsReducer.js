d3f50bafb0611256b8e7c2b6b0a8fd040905f8c7
Lucas Ebert <lucascello@yahoo.com.br>
2022-04-08 13:20:11 +0200

    displaying each user on the post

--- blog/src/reducers/postsReducer.js
+++ blog/src/reducers/postsReducer.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-anonymous-default-export
 export default (state = [], action) => {
     // if (action.type === "FETCH_POSTS") {
     //     return action.payload;
