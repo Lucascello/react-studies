08ee571e454cb388bd91af7fa0cc1960f21ef1f2
Lucas Ebert <lucascello@yahoo.com.br>
2022-04-11 16:56:18 +0200

    understanding the difference between BrowserRouter, HashRouter and MemoryRouter

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,26 +1,73 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
+////////////////////////////////////////////////////////////////////////////////////
+
+// Instead of BrowserRouter, you can also use the HashRouter. It puts a hash(#) symbol between the slashes(/). In this case the requests are always made to (in this case) localhost:3000 and then looks to find what is after the #. It doesn't require special configuration by the backend server.
+// import { HashRouter, Route, Link } from "react-router-dom";
+
+////////////////////////////////////////////////////////////////////////////////////
+
+// You can also use the MemoryRouter. It doesn't update the URL. 
+// import { MemoryRouter, Route, Link } from "react-router-dom";
+
+///////////////////////////////////////////////////////////////////////////////////
 
 const PageOne = () => {
-    return <div>Page One</div>;
+    return (
+        <div>
+            Page One
+            <Link to="/pagetwo">Navigate to page 2</Link>
+        </div>
+    );
 };
 
 const PageTwo = () => {
-    return <div>Page Two</div>;
+    return (
+        <div>
+            Page Two
+            <Link to="/">Navigate to page 1</Link>
+        </div>
+    );
 };
 
 const App = () => {
     return (
         <div>
             <BrowserRouter>
-            <div>
-                <Route path="/" exact component={PageOne} />
-                <Route path="/pagetwo" component={PageTwo} />
-            </div>
-                
+                <div>
+                    <Route path="/" exact component={PageOne} />
+                    <Route path="/pagetwo" component={PageTwo} />
+                </div>
             </BrowserRouter>
         </div>
     );
 };
+
+// const App = () => {
+//     return (
+//         <div>
+//             <HashRouter>
+//                 <div>
+//                     <Route path="/" exact component={PageOne} />
+//                     <Route path="/pagetwo" component={PageTwo} />
+//                 </div>
+//             </HashRouter>
+//         </div>
+//     );
+// };
+
+// const App = () => {
+//     return (
+//         <div>
+//             <MemoryRouter>
+//                 <div>
+//                     <Route path="/" exact component={PageOne} />
+//                     <Route path="/pagetwo" component={PageTwo} />
+//                 </div>
+//             </MemoryRouter>
+//         </div>
+//     );
+// };
 
 export default App;
