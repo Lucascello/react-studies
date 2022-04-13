cd0761bbda7073c0ef0337c6c3d73b60e857dbc1
Lucas Ebert <lucascello@yahoo.com.br>
2022-04-13 14:19:04 +0200

    created some logic for the sign in

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -1,18 +1,35 @@
 import React from "react";
 
 class GoogleAuth extends React.Component {
+    state = { isSignedIn: null };
+
     componentDidMount() {
         window.gapi.load("client:auth2", () => {
-            window.gapi.client.init({
-                clientId:
-                    "266396411988-kbb12buq51muo2j0nucfsr0qd3eb0066.apps.googleusercontent.com",
-                scope: "email",
-            });
+            window.gapi.client
+                .init({
+                    clientId:
+                        "266396411988-kbb12buq51muo2j0nucfsr0qd3eb0066.apps.googleusercontent.com",
+                    scope: "email",
+                })
+                .then(() => {
+                    this.auth = window.gapi.auth2.getAuthInstance();
+                    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+                });
         });
     }
 
+    renderAuthButton() {
+        if (this.state.isSignedIn === null) {
+            return <div>Maybe you're not signed in</div>;
+        } else if (this.state.isSignedIn) {
+            return <div>You're signed in</div>;
+        } else {
+            return <div>You're not signed in</div>;
+        }
+    }
+
     render() {
-        return <div>Google Auth</div>;
+        return <div>{this.renderAuthButton()}</div>;
     }
 }
 
