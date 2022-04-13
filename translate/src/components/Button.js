25460515c8c45aa925a2c036a484cbee41010a78
Lucas Ebert <lucascello@yahoo.com.br>
2022-04-13 20:44:29 +0200

    passing context through the components

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -1,9 +1,17 @@
 import React from "react";
+import LanguageContext from "../context/LanguageContext";
 
 class Button extends React.Component {
+    static contextType = LanguageContext;
+
     render() {
-        return <button className="ui button primary">Submit</button>;
+        const text = this.context === "english" ? "Submit" : "Voorleggen";
+
+        return <button className="ui button primary">{text}</button>;
     }
 }
+
+///////// The code below is the same as static contextType = LanguageContext; ///////
+//Button.contextType = LanguageContext;
 
 export default Button;
