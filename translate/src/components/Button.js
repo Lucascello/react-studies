19c392e242730fdb1b8b0ee712fc8783425ab001
Lucas Ebert <lucascello@yahoo.com.br>
2022-04-13 21:02:02 +0200

    passing down context with the Consumer

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -2,12 +2,18 @@ import React from "react";
 import LanguageContext from "../context/LanguageContext";
 
 class Button extends React.Component {
-    static contextType = LanguageContext;
+    // static contextType = LanguageContext;
 
     render() {
-        const text = this.context === "english" ? "Submit" : "Voorleggen";
+        // const text = this.context === "english" ? "Submit" : "Voorleggen";
 
-        return <button className="ui button primary">{text}</button>;
+        return (
+            <button className="ui button primary">
+                <LanguageContext.Consumer>
+                    {(value) => (value === "english" ? "Submit" : "Voorleggen")}
+                </LanguageContext.Consumer>
+            </button>
+        );
     }
 }
 
