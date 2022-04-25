7b6ce941db2a557bc5003e379496fdc3907cc9f3
Lucas Ebert <lucascello@yahoo.com.br>
2022-04-25 17:31:17 +0200

    using formProps

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -2,12 +2,25 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
+    renderInput(formProps) {
+        //console.log(formProps); form props coming from redux-form
+        return (
+            // <input
+            //     onChange={formProps.input.onChange}
+            //     value={formProps.input.value}
+            // />
+            // better way of writting the code above
+
+            <input {...formProps.input} />
+        );
+    }
+
     render() {
         // console.log(this.props); -> props coming from redux-form
         return (
             <form>
-                <Field name="title"/>
-                <Field name="description"/>
+                <Field name="title" component={this.renderInput} />
+                <Field name="description" component={this.renderInput} />
             </form>
         );
     }
