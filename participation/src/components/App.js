8687ed3be19ab0941dbab725082cf79273629a61
Lucas Ebert <lucascello@yahoo.com.br>
2022-04-26 18:14:44 +0200

    header component

--- participation/src/components/App.js
+++ participation/src/components/App.js
@@ -1,72 +1,72 @@
 import React from "react";
+import Header from "./Header";
 
 const App = () => {
     return (
-        <table class="ui selectable celled table">
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Lucas</td>
-                    <td>No Action</td>
-                    <td>None</td>
-                </tr>
-                <tr>
-                    <td>Mathias</td>
-                    <td>Approved</td>
-                    <td>Requires call</td>
-                </tr>
-                <tr>
-                    <td>Kenya</td>
-                    <td>Denied</td>
-                    <td>None</td>
-                </tr>
-                <tr class="warning">
-                    <td>Vilma</td>
-                    <td>No Action</td>
-                    <td>None</td>
-                </tr>
-                <tr>
-                    <td>Charity</td>
-                    <td class="positive">Approved</td>
-                    <td class="warning">Requires call</td>
-                </tr>
-                <tr>
-                    <td>Filipe</td>
-                    <td class="negative">Denied</td>
-                    <td>None</td>
-                </tr>
-                <tr>
-                    <td>Andrei</td>
-                    <td class="negative">Denied</td>
-                    <td>None</td>
-                </tr>
-                <tr>
-                    <td>Danilo</td>
-                    <td class="negative">Denied</td>
-                    <td>None</td>
-                </tr>
-                <tr>
-                    <td>Debora</td>
-                    <td class="negative">Denied</td>
-                    <td>None</td>
-                </tr>
-                <tr>
-                    <td>Erika</td>
-                    <td class="negative">Denied</td>
-                    <td>None</td>
-                </tr>
-                <tr>
-                    <td>Foster</td>
-                    <td class="negative">Denied</td>
-                    <td>None</td>
-                </tr>
-            </tbody>
-        </table>
+        <>
+            <br />
+            <div>Praise Team Sunday Service Participation</div>
+            <table class="ui selectable celled table">
+                <Header />
+                <tbody>
+                    <tr>
+                        <td>Lucas</td>
+                        <td>No Action</td>
+                        <td>None</td>
+                    </tr>
+                    <tr>
+                        <td>Mathias</td>
+                        <td>Approved</td>
+                        <td>Requires call</td>
+                    </tr>
+                    <tr>
+                        <td>Kenya</td>
+                        <td>Denied</td>
+                        <td>None</td>
+                    </tr>
+                    <tr>
+                        <td>Vilma</td>
+                        <td>No Action</td>
+                        <td>None</td>
+                    </tr>
+                    <tr>
+                        <td>Charity</td>
+                        <td class="positive">Approved</td>
+                        <td class="warning">Requires call</td>
+                    </tr>
+                    <tr>
+                        <td>Filipe</td>
+                        <td class="negative">Denied</td>
+                        <td>None</td>
+                    </tr>
+                    <tr>
+                        <td>Andrei</td>
+                        <td class="negative">Denied</td>
+                        <td>None</td>
+                    </tr>
+                    <tr>
+                        <td>Danilo</td>
+                        <td class="negative">Denied</td>
+                        <td>None</td>
+                    </tr>
+                    <tr>
+                        <td>Debora</td>
+                        <td class="negative">Denied</td>
+                        <td>None</td>
+                    </tr>
+                    <tr>
+                        <td>Erika</td>
+                        <td class="negative">Denied</td>
+                        <td>None</td>
+                    </tr>
+                    <tr>
+                        <td>Foster</td>
+                        <td class="negative">Denied</td>
+                        <td>None</td>
+                    </tr>
+                </tbody>
+            </table>
+        </>
     );
 };
 
---
+++ participation/src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Header = () => {
+    return (
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>May 1st</th>
+                    <th>May 8th</th>
+                    <th>May 15th</th>
+                    <th>May 22nd</th>
+                    <th>May 29th</th>
+                    <th>June 5th</th>
+                    <th>June 12th</th>
+                    <th>June 19th</th>
+                    <th>June 26th</th>
+                </tr>
+            </thead>
+    );
+};
+
+export default Header;
