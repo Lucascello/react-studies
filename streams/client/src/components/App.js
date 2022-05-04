eb1c25bcb8fb931c3d6f3e84a1b3c67f2ad8bb84
Lucas Ebert <lucascello@yahoo.com.br>
2022-05-04 12:17:23 +0200

    created history component, and exchanged BrowserRouter for Router on the App component

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { /*BrowserRouter*/ Router, Route } from "react-router-dom";
 import Header from "./Header";
 import StreamCreate from "./streams/StreamCreate";
 import StreamDelete from "./streams/StreamDelete";
 import StreamEdit from "./streams/StreamEdit";
 import StreamList from "./streams/StreamList";
 import StreamShow from "./streams/StreamShow";
+import history from "../history";
 
 ////////////////////////////////////////////////////////////////////////////////////
 
@@ -22,7 +23,7 @@ import StreamShow from "./streams/StreamShow";
 const App = () => {
     return (
         <div className="ui container">
-            <BrowserRouter>
+            <Router history={history}>
                 <div>
                     <Header />
                     <Route path="/" exact component={StreamList} />
@@ -35,7 +36,7 @@ const App = () => {
                     />
                     <Route path="/streams/show" exact component={StreamShow} />
                 </div>
-            </BrowserRouter>
+            </Router>
         </div>
     );
 };
