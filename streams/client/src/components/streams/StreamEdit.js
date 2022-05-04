4bddd3af87b22a37fe8cc2c83a2f856e1c95ec30
Lucas Ebert <lucascello@yahoo.com.br>
2022-05-04 14:48:49 +0200

    refactored StreaEdit component and the action creator for it to a patch request

--- streams/client/src/components/streams/StreamEdit.js
+++ streams/client/src/components/streams/StreamEdit.js
@@ -1,6 +1,8 @@
+import _ from "lodash";
 import React from "react";
 import { connect } from "react-redux";
-import { fetchStream } from "../../actions";
+import { fetchStream, editStream } from "../../actions";
+import StreamForm from "./StreamForm";
 
 // const StreamEdit = (props) => {
 //     //console.log(props);
@@ -12,12 +14,33 @@ class StreamEdit extends React.Component {
         this.props.fetchStream(this.props.match.params.id);
     }
 
+    onSubmit = (formValues) => {
+        //console.log(formValues);
+        this.props.editStream(this.props.match.params.id, formValues);
+    };
+
     render() {
         //console.log(this.props);
         if (!this.props.stream) {
             return <div>Loading...</div>;
         }
-        return <div>{this.props.stream.title}</div>;
+        return (
+            <div>
+                <h3>Edit Your Stream</h3>
+                <StreamForm
+                    // initialValues={{
+                    //     title: "Edit Me",
+                    //     description: "Change Me Too",
+                    // }}
+                    initialValues={_.pick(
+                        this.props.stream,
+                        "title",
+                        "description"
+                    )}
+                    onSubmit={this.onSubmit}
+                />
+            </div>
+        );
     }
 }
 
@@ -26,4 +49,6 @@ const mapStateToProps = (state, ownProps) => {
     return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default connect(mapStateToProps, { fetchStream })(StreamEdit);
+export default connect(mapStateToProps, { fetchStream, editStream })(
+    StreamEdit
+);
