143adc677b610ac7d520b710751336bc3d38ed51
Lucas Ebert <lucascello@yahoo.com.br>
2022-05-05 16:28:43 +0200

    modal component is dynamic now

--- streams/client/src/components/streams/StreamDelete.js
+++ streams/client/src/components/streams/StreamDelete.js
@@ -2,10 +2,31 @@ import React from "react";
 import Modal from "../Modal";
 
 const StreamDelete = () => {
+    // My Solution to the extra div (See Modal.js for the entire picture)
+    // const actions = (
+    //     <div className="actions">
+    //         <button className="ui button negative">Delete</button>
+    //         <button className="ui button">Cancel</button>
+    //     </div>
+    // );
+
+    const actions = (
+        // <React.Fragment> same as line below
+        <>
+            <button className="ui button negative">Delete</button>
+            <button className="ui button">Cancel</button>
+        </>
+        // </React.Fragment>
+    );
+
     return (
         <div>
             Stream Delete
-            <Modal />
+            <Modal
+                title="Delete Stream"
+                content="Are you sure you want to delete this stream?"
+                actions={actions}
+            />
         </div>
     );
 };
