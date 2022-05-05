fcf09120efbabb109dca896d0ab5410bda3a5ec9
Lucas Ebert <lucascello@yahoo.com.br>
2022-05-05 16:52:27 +0200

    refactored StreamDelete into a class component

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -34,7 +34,7 @@ const App = () => {
                         component={StreamEdit}
                     />
                     <Route
-                        path="/streams/delete"
+                        path="/streams/delete/:id"
                         exact
                         component={StreamDelete}
                     />
--- streams/client/src/components/streams/StreamDelete.js
+++ streams/client/src/components/streams/StreamDelete.js
@@ -1,36 +1,49 @@
 import React from "react";
+import { connect } from "react-redux";
 import Modal from "../Modal";
 import history from "../../history";
+import { fetchStream } from "../../actions";
 
-const StreamDelete = () => {
+class StreamDelete extends React.Component {
+    componentDidMount() {
+        //console.log(this.props);
+        this.props.fetchStream(this.props.match.params.id);
+    }
+
     // My Solution to the extra div (See Modal.js for the entire picture)
-    // const actions = (
-    //     <div className="actions">
+    // renderActions() {
+    //     return (
+    //         <div className="actions">
     //         <button className="ui button negative">Delete</button>
     //         <button className="ui button">Cancel</button>
     //     </div>
-    // );
+    //     );
+    // }
 
-    const actions = (
-        // <React.Fragment> same as line below
-        <>
-            <button className="ui button negative">Delete</button>
-            <button className="ui button">Cancel</button>
-        </>
-        // </React.Fragment>
-    );
+    renderActions() {
+        return (
+            // <React.Fragment> same as line below
+            <>
+                <button className="ui button negative">Delete</button>
+                <button className="ui button">Cancel</button>
+            </>
+            // </React.Fragment>
+        );
+    }
 
-    return (
-        <div>
-            Stream Delete
-            <Modal
-                title="Delete Stream"
-                content="Are you sure you want to delete this stream?"
-                actions={actions}
-                onDismiss={() => history.push("/")}
-            />
-        </div>
-    );
-};
+    render() {
+        return (
+            <div>
+                Stream Delete
+                <Modal
+                    title="Delete Stream"
+                    content="Are you sure you want to delete this stream?"
+                    actions={this.renderActions()}
+                    onDismiss={() => history.push("/")}
+                />
+            </div>
+        );
+    }
+}
 
-export default StreamDelete;
+export default connect(null, { fetchStream })(StreamDelete);
